008ec989acda7b1ff18fc4aaf06dafe89d3e3939
Lindist <four181049@gmail.com>
2026-03-14 00:09:29 +0700

    fix issue

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,12 +1,13 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { MongoClient } from "mongodb"; // your mongodb client
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { initUserBoard } from "../init-user-board";
+import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI!)
-const db = client.db()
+const client = new MongoClient(process.env.MONGODB_URI!);
+const db = client.db();
+
 export const auth = betterAuth({
     database: mongodbAdapter(db, {
         client,
